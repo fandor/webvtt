17f9a87e5e72d0225abd9dd1d8a4b04475a727af
Sam Dornan <sdornan@gmail.com>
2013-09-25 15:26:24 -0700

    Use a space instead

--- lib/webvtt/file.rb
+++ lib/webvtt/file.rb
@@ -66,7 +66,7 @@ private
         cue_opts[:identifier] = collected_lines.first
         cue_opts[:cue_line] = collected_lines[1]
       end
-      cue_opts[:text] = collected_lines[2..-1].join("\n")
+      cue_opts[:text] = collected_lines[2..-1].join(' ')
       cues << Cue.new(cue_opts)
     end
 
